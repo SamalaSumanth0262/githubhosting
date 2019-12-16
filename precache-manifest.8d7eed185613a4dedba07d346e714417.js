fee0cfffd0446d63221fac20acf2a725fb5071ba
System Administrator <sumanth.samala@letsventure.com>
2019-12-16 14:23:30 +0530

    Updates

---
+++ precache-manifest.8d7eed185613a4dedba07d346e714417.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6238941c94ebcff65abb534675578e4f",
+    "url": "/githubhosting/index.html"
+  },
+  {
+    "revision": "cbd42149a69272899f44",
+    "url": "/githubhosting/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d96d4c2d21a018799a1e",
+    "url": "/githubhosting/static/js/2.ef0a6cc1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/githubhosting/static/js/2.ef0a6cc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cbd42149a69272899f44",
+    "url": "/githubhosting/static/js/main.253b386b.chunk.js"
+  },
+  {
+    "revision": "09cdba7343308615a37b",
+    "url": "/githubhosting/static/js/runtime-main.5e11b98f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubhosting/static/media/logo.5d5d9eef.svg"
+  }
+]);
